3598bc2589a72665af1ee986f7705c5181a54b9f
Moaz Hamza <mhamza1@binghamton.edu>
2018-04-30 13:40:58 -0400

    Attempt at logging initialization

--- Key-ValueStoreServer.py
+++ Key-ValueStoreServer.py
@@ -1,19 +1,39 @@
-class Store:
-    def __init__(self):
-        self.store = []
-        # TODO: Look for your logging file, populate the memory if it exists, create one if it does not
+import os
 
-    def populate_from_mem(self, log):
+
+class Store:
+    def __init__(self, log_file=None):
+        self.store = []
+        self.log = None
+        if log_file:
+            for root, _, files in os.walk(""):
+                for file in files:
+                    if file == log_file:
+                        self.log = open(file, 'r')
+                        self.__populate_from_mem(self.log)
+                if self.log is None:
+                    self.log = open(log_file, 'rw+')
+
+    def __populate_from_mem(self, log):
         # TODO: Read through the logger, populating the memory as you go
-        pass
+        for line in log:
+            # TODO: Populate the store using the lines of the file (Need to settle on format to write to the file
+            pass
 
     def get(self, key):
+        assert type(key) == int
+        assert key >= 0
+        assert key <= 255
         if key in self.store:
             return self.store[key]
         else:
             return False
 
     def put(self, key, value):
+        assert type(key) == int
+        assert type(value) == str
+        assert key >= 0
+        assert key <= 255
         # TODO: Write in logger files all writes committed to the store
         if key in self.store:
             self.store[key] = value
